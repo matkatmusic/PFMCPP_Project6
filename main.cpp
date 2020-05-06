94056d7bb8e942c157c82ca38a37b9407ccc32e4
Russyallop <russyallop@gmail.com>
2020-05-06 17:33:21 +0000

    Check over

--- main.cpp
+++ main.cpp
@@ -1,45 +1,45 @@
 /*
  Project 6: Part 1 / 2
  Video: Chapter 3 Part 2
-
-Create a branch named Part1
-
-Purpose:  This project will show you the difference between member functions and static class functions, as well as the difference between pointers and references and the code semantics that accompany pointers and references.
-
+ 
+ Create a branch named Part1
+ 
+ Purpose:  This project will show you the difference between member functions and static class functions, as well as the difference between pointers and references and the code semantics that accompany pointers and references.
+ 
  Pointers
-
+ 
  Please look at the screenshot in the files to see what happens if you paste this assignment into an existing cpp file in an xcode project
  
  Task: replace all of the <place holder> with proper names and types that will make this program compile and run.
  
  press the Compile, link and run... button and work your way through all of the error messages.
  
-
+ 
  steps:
  
  1) finish implementing the constructor for T. the constructor parameters need to initialize 2) and 3)
  
  2) provide a member variable in T of a type of your choosing that works with the compare function below T
-         note the name of the variable used in the compare function
+ note the name of the variable used in the compare function
  
  3) provide a member variable of type std::string in T that works with the *first* cout statement in main().
-         note the name of the variable used in the *first* cout statement in main()
+ note the name of the variable used in the *first* cout statement in main()
  
  4) finish defining the struct with the compare function
  5) provide proper types for the arguments to compare, and a proper return value
-         be sure to make it work with the pointer semantics being used in the if statements.
-         see 8) for a hint about what type to use.
+ be sure to make it work with the pointer semantics being used in the if statements.
+ see 8) for a hint about what type to use.
  
  6) finish instantiating the two T instances being declared in main(), based on the constructor you finished implementing in T
-     Their names (line 105,106) are different than the member variables names (line 77), even tho the placeholders share the same name.
-     Don't let this confuse you.
+ Their names (line 105,106) are different than the member variables names (line 77), even tho the placeholders share the same name.
+ Don't let this confuse you.
  
  7) Declare 'f' with the proper type so the call to f.compare works
  
  8) call f.compare() with the correct arguments, based on 2) and 5)
  
  9) correct the cout that uses smaller's member variable with some safe pointer usage.
-         the possible return values of the compare() function should give you a hint.
+ the possible return values of the compare() function should give you a hint.
  
  10) complete the implementation for the static function in <structName2>
  
@@ -53,19 +53,18 @@ Purpose:  This project will show you the difference between member functions and
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
-Make a pull request after you make your first commit and pin the pull request link to our DM thread.
-
-send me a DM to check your pull request
-
+ Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+ 
+ send me a DM to check your pull request
+ 
  Wait for my code review.
  */
-
 #include <cmath>
 #include <iostream>
 #include <string>
 struct T
 {
-    T(float v, const char* name_) : value(v), name(name_) {}  //1
+    T(float v, const char* name) : value(v), name(name) {}  //1
     float value;
     std::string name;
 };
@@ -82,32 +81,31 @@ struct T2                                //4
 
 struct U
 {
-    float leadingValue { 0.0f }, trailingValue { 0.0f };
-    float uSquare(float* leadingValueIn)      //12
+    float targetValue { 0.0f }, uValue { 0.0f };
+    float uSquare(float* targetValueIn)      //12
     {
-        leadingValue = *leadingValueIn;
-        while( std::abs(trailingValue - leadingValue) > 0.001f )
+        targetValue = *targetValueIn;
+        while( std::abs(uValue - targetValue) > 0.001f )
         {
-            trailingValue += -0.5*(trailingValue - leadingValue);
+            uValue += -0.5*(uValue - targetValue);
         }
-        return trailingValue * trailingValue;
-        
+        return uValue * uValue;
     }
 };
 
 struct W
 {
-    static float wSquare(U* that, float* leadingValue )        //10
+    static float wSquare(U* that, float* targetValue )        //10
     {
-        std::cout << "U's leadingValue value: " << that->leadingValue << std::endl;
-        that->leadingValue = *leadingValue;
-        std::cout << "U's leadingValue updated value: " << that->leadingValue << std::endl;
-        while( std::abs(that->trailingValue - that->leadingValue) > 0.001f )
+        std::cout << "U's targetValue value: " << that->targetValue << std::endl;
+        that->targetValue = *targetValue;
+        std::cout << "U's targetValue updated value: " << that->targetValue << std::endl;
+        while( std::abs(that->uValue - that->targetValue) > 0.001f )
         {
-            that->trailingValue += -0.5*(that->trailingValue - that->leadingValue);
+            that->uValue += -0.5*(that->uValue - that->targetValue);
         }
-        std::cout << "U's trailingValue updated value: " << that->trailingValue << std::endl;
-        return that->trailingValue * that->trailingValue;
+        std::cout << "U's uValue updated value: " << that->uValue << std::endl;
+        return that->uValue * that->uValue;
     }
 };
         
@@ -117,8 +115,12 @@ int main()
     T value2(5 ,"five" );                                             //6
     
     T2 f;                                            //7
-    auto* smaller = f.compare(&value1 ,&value2 );                              //8
-    std::cout << "the smaller one is << " << (value1.value==value2.value? "neither; they are the same." : smaller->name) << std::endl; //9
+    auto* smaller = f.compare(&value1 ,&value2 );
+    if(smaller!=nullptr)
+    {//8
+    std::cout << "the smaller one is << " << (value1.value==value2.value ? "neither; they are the same." : smaller->name) << std::endl;
+    }
+    //9
     
     U u1;
     float updatedValue = 5.f;
