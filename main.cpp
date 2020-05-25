f089d4c418de83c644a4248e66f0b9e007b3188e
matkatmusic <matkatmusic@gmail.com>
2020-05-25 14:28:16 -0700

    added part 2

--- main.cpp
+++ main.cpp
@@ -1,56 +1,21 @@
 /*
- Project 6: Part 1 / 2
- Video: Chapter 3 Part 2
+ Project 6: Part 2 / 2
+ Video: Chapter 3 Part 3
 
-Create a branch named Part1
+Create a branch named Part2
 
-Purpose:  This project will show you the difference between member functions and static class functions, as well as the difference between pointers and references and the code semantics that accompany pointers and references.
-
- Pointers
-
- Please look at the screenshot in the files to see what happens if you paste this assignment into an existing cpp file in an xcode project
+ References
  
- Task: replace all of the <place holder> with proper names and types that will make this program compile and run.
  
- press the Compile, link and run... button and work your way through all of the error messages.
+ 1) convert all of the pointer usage (except for 'const char*') to reference types or 
+    const reference types **>>> WHERE POSSIBLE <<<**
+        hint: There is no reference equivalent to nullptr.  
+        if a pointer (including nullptr) is being returned anywhere, don't try to convert it to a reference.
  
-
- steps:
+ 2) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
  
- 1) finish implementing the constructor for T. the constructor parameters need to initialize 2) and 3)
- 
- 2) provide a member variable in T of a type of your choosing that works with the compare function below T
-         note the name of the variable used in the compare function
- 
- 3) provide a member variable of type std::string in T that works with the *first* cout statement in main().
-         note the name of the variable used in the *first* cout statement in main()
- 
- 4) finish defining the struct with the compare function
- 5) provide proper types for the arguments to compare, and a proper return value
-         be sure to make it work with the pointer semantics being used in the if statements.
-         see 8) for a hint about what type to use.
- 
- 6) finish instantiating the two T instances being declared in main(), based on the constructor you finished implementing in T
-     Their names (line 105,106) are different than the member variables names (line 77), even tho the placeholders share the same name.
-     Don't let this confuse you.
- 
- 7) Declare 'f' with the proper type so the call to f.compare works
- 
- 8) call f.compare() with the correct arguments, based on 2) and 5)
- 
- 9) correct the cout that uses smaller's member variable with some safe pointer usage.
-         the possible return values of the compare() function should give you a hint.
- 
- 10) complete the implementation for the static function in <structName2>
- 
- 11) call the <structName2> static function correctly in main()
- 
- 12) replicate the functionality of <structName2>'s static function by implementing a member function in U that does the same thing.
- 
- 13) remember the rules for using pointers!  What is the one thing we always do before we use a pointer?
- 
- After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
  */
+
 
 #include <iostream>
 #include <string>
